56155a39b7809c5185ae8d620f7698dc8b305a32
Thiago Miziara <thiagonmiziara@gmail.com>
2021-10-20 19:44:34 -0300

    add fonts external and configs

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles as S } from "./styles";
+
+export function Home() {
+  return (
+    <View style={S.container}>
+      <Text>Olá React Native</Text>
+    </View>
+  );
+}
